2d1e11af4779a9e3712e90d8d2b137480905c7cd
MVPlauda <plaudadev@gmail.com>
2023-04-29 01:37:28 -0300

    subtraindo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        float num1, num2, subtração;
+
+        Console.Write("Digite um numero: ");
+        num1 = float.Parse(Console.ReadLine());
+        Console.Write("Digite outro numero: ");
+        num2 = float.Parse(Console.ReadLine());
+
+        subtração = num1 - num2;
+
+        Console.WriteLine($"A subtração entre os 2 numero é: {subtração}");
+
+    }
+}
